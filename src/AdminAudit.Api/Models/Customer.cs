765bc004ec63aa21a8fa2efd1850959c90c211e7
Tshimangadzo <tshimangadzostephen@gmail.com>
2026-01-30 14:10:39 +0200

    customer model

--- src/AdminAudit.Api/Models/Customer.cs
+++ src/AdminAudit.Api/Models/Customer.cs
@@ -2,5 +2,9 @@
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Status { get; set; } = "ACTIVE";
+        public string? Notes { get; set; }
     }
 }

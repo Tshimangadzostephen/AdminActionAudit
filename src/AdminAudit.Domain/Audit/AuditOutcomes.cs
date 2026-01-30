d8c6a05f31e76a5f30f837373545459f47acfae7
Tshimangadzo <tshimangadzostephen@gmail.com>
2026-01-30 14:11:09 +0200

    audit outcomes

---
+++ src/AdminAudit.Domain/Audit/AuditOutcomes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminAudit.Domain.Audit
+{
+    public static class AuditOutcomes
+    {
+        public const string Success = "SUCCESS";
+        public const string Failed = "FAILED";
+    }
+}
